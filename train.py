f5cb1997681cee09924f39c67f8ce0037639ed92
tonydp03 <tony.dipilato03@gmail.com>
2019-07-03 16:20:11 +0200

    Testing more models

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import keras
 from keras.models import Model
 from keras.callbacks import EarlyStopping
-from keras.layers import Input, Conv2D, MaxPooling2D, Concatenate, BatchNormalization, Flatten, Dense
+from keras.layers import Input, Conv2D, MaxPooling2D, Concatenate, BatchNormalization, Flatten, Dense, Dropout
 from keras import optimizers
 
 import os
@@ -19,11 +19,11 @@ import pandas as pd
 from keras import backend as K
 from pdb import set_trace
 
-batch_size = 1024
+batch_size = 256
 channels = 3
 img_height = 30
 img_width = 50
-classes = 6
+classes = 5 #6
 epochs = 15
 dataset_dir = 'data/pkl/'
 save_dir = 'saved_models/'
@@ -35,6 +35,11 @@ train_dat = pd.read_pickle(dataset_dir + "dataset_train.pkl")
 test_dat = pd.read_pickle(dataset_dir + "dataset_test.pkl")
 
 print('Loading training data...')
+train_dat = train_dat.query("label!=3")
+slt = train_dat.label>3
+train_dat.loc[slt,"label"] -= 1
+train_dat.reset_index(drop=True,inplace=True)
+
 x_train, pid_train, en_train = [],[], []
 for i in range(len(train_dat)):
 #     print('Event number {}'.format(i))
@@ -44,7 +49,6 @@ for i in range(len(train_dat)):
 x_train = np.array(x_train)
 pid_train = np.array(pid_train)
 en_train = np.array(en_train)
-# x_train = x_train.reshape(-1,channels,img_height,img_width)
 pid_train = keras.utils.to_categorical(pid_train, num_classes=classes, dtype='float32')
 print('Training data loaded!')
 
@@ -53,16 +57,19 @@ print(pid_train.shape)
 print(en_train.shape)
 
 print('Loading test data...')
+test_dat = test_dat.query("label!=3")
+slt = test_dat.label>3
+test_dat.loc[slt,"label"] -= 1
+test_dat.reset_index(drop=True,inplace=True)
+
 x_test, pid_test, en_test = [],[], []
 for i in range(len(test_dat)):
-#     print('Event number {}'.format(i))
     x_test.append(test_dat.loc[i].feature)
     pid_test.append(test_dat.loc[i].label)
     en_test.append(test_dat.loc[i].gen_energy)
 x_test = np.array(x_test)
 pid_test = np.array(pid_test)
 en_test = np.array(en_test)
-# x_test = x_test.reshape(-1,channels,img_height,img_width)
 pid_test = keras.utils.to_categorical(pid_test, num_classes=classes, dtype='float32')
 print('Test data loaded!')
 
@@ -70,54 +77,61 @@ print(x_test.shape)
 print(pid_test.shape)
 print(en_test.shape)
 
-# x_val,y_val = [],[]
-# for i in range(len(val_dat)):
-#     x_val.append(val_dat.loc[i].feature)
-#     y_val.append(val_dat.loc[i].label)
-# x_val = np.array(x_val)
-# y_val = np.array(y_val)
-# x_val = x_val.reshape(-1,channels,52)
-# y_val = keras.utils.to_categorical(y_val, num_classes=classes, dtype='float32')
-
-# print(x_val.shape)
-# print(y_val.shape)
-
 
 def shower_classification_model():
         input_img = Input(shape=(img_width, img_height, channels), name='input')
-        conv = Conv2D(3, (1,1), padding='same', data_format='channels_last', name='conv0')(input_img)
-        bnorm = BatchNormalization()(input_img)
 
-        conv = Conv2D(3, (5,1), activation='relu', padding='same', data_format='channels_last', name='conv1')(conv)
-#    bnorm = BatchNormalization()(conv)
-#    pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool1')(bnorm)
-
-        conv = Conv2D(6, (5,5), activation='relu', padding='same', data_format='channels_last', name='conv2')(conv)
-#    bnorm = BatchNormalization()(conv)
-#    pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool2')(bnorm)
-
-        conv = Conv2D(12, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv3')(conv)
-#    bnorm = BatchNormalization()(conv)
-#    pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool3')(bnorm)
-
-#     bnorm = BatchNormalization()(conv)
-#     pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool1')(bnorm)
-
-#     conv = Conv2D(64, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv3')(pool)
-#     conv = Conv2D(64, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv4')(conv)
-#     bnorm = BatchNormalization()(conv)
-#     pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool2')(bnorm)
+        conv = Conv2D(3, (1,1), padding='same', data_format='channels_last', name='conv1')(input_img)
+        conv = Conv2D(16, (5,1), activation='relu', padding='same', data_format='channels_last', name='conv2')(conv)
+        conv = Conv2D(32, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv3')(conv)
+        conv = Conv2D(64, (4,4), activation='relu', padding='same', data_format='channels_last', name='conv4')(conv)
 
         flat = Flatten()(conv)
+        # bnorm = BatchNormalization()(flat)
 
-        dense = Dense(128, activation='relu', name='dense1')(flat)
-#    dense = Dense(32, activation='sigmoid', name='dense2')(dense)
-        pred = Dense(classes, activation='softmax', name='output')(dense)
+        dense = Dense(1024, activation='relu', name='dense1')(flat)
+        # drop = Dropout(0.5)(dense)
+        dense = Dense(128, activation='relu', name='dense2')(dense)#(drop)
+        # drop = Dropout(0.2)(dense)
+
+        pred = Dense(classes, activation='softmax', name='output')(dense)#(drop)
 
         model = Model(inputs=input_img, outputs=pred)
 
-
+        opt = optimizers.Adam(lr=0.01)
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
+
+def shower_classification_model_V2():
+        input_img = Input(shape=(img_width, img_height, channels), name='input')
+
+        l1 = Conv2D(3, (1,1), padding='same', data_format='channels_last', name='conv1')(input_img)
+
+        l2 = Conv2D(5, (1,1), activation='relu', padding='same', data_format='channels_last', name='conv2')(input_img)
+        l2 = Conv2D(5, (5,1), activation='relu', padding='same', data_format='channels_last', name='conv3')(l2)
+        
+        l3 = Conv2D(5, (1,1), activation='relu', padding='same', data_format='channels_last', name='conv4')(input_img)
+        l3 = Conv2D(5, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv5')(l3)
+
+        l4 = MaxPooling2D(pool_size=(3, 3), strides=(1,1), padding='same', data_format='channels_last', name='pool1')(input_img)
+        l4 = Conv2D(5, (1,1), activation='relu', padding='same', data_format='channels_last', name='conv6')(l4)
+
+        conc = Concatenate(axis=3)([l1,l2,l3,l4])
+
+        flat = Flatten()(conc)
+        # bnorm = BatchNormalization()(flat)
+
+        dense = Dense(1024, activation='relu', name='dense1')(flat)
+        # drop = Dropout(0.5)(dense)
+        dense = Dense(128, activation='relu', name='dense2')(drop)
+        # drop = Dropout(0.2)(dense)
+
+        pred = Dense(classes, activation='softmax', name='output')(drop)
+
+        model = Model(inputs=input_img, outputs=pred)
+
+        opt = optimizers.Adam(lr=0.01)
+        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
         return model
 
 print('Creating model...')
