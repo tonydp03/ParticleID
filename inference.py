d7aa0e36bb0f8601d9198a13db78c0c520704ca1
tonydp03 <tony.dipilato03@gmail.com>
2019-07-05 10:32:37 +0200

    Faster dataset reading and energy regression model included

--- inference.py
+++ inference.py
@@ -17,35 +17,70 @@ import os
 import numpy as np
 import pandas as pd
 
-classes = 5 #6
+classes = 4 #5 #6
 dataset_dir = 'data/pkl/'
 save_dir = 'saved_models/'
-model_name = 'cnn_v1'
+model_name = 'full_model' #'pid'  #cnn_v1'
 plotdir = 'plots/'
 
 # Load data
+print('Opening pickle...')
 test_dat = pd.read_pickle(dataset_dir + "dataset_test.pkl")
-print('Loading test data...')
+print('Pickle read!')
 
-test_dat = test_dat.query("label!=3")
-slt = test_dat.label>3
-test_dat.loc[slt,"label"] -= 1
+# x_test =[]
+# print('x_test:  ', x_test[2])
+# print('x_test[0]:  ', x_test[0])
+# print('x_test[0] shape:  ', x_test[0].shape)
+# print('shape: ', x_test.shape)
+# pid_test = test_dat.label.values
+# print('shape: ', pid_test.shape)
+# en_test = test_dat.gen_energy.values
+# print('shape: ', en_test.shape)
+
+print('Loading test data...')
+test_dat = test_dat.query("label!=3") #drop tau
+test_dat = test_dat.query("label!=4") #drop pi0
+# slt = test_dat.label>3
+slt = test_dat.label==5
+# slt2 = (test_dat.label==1) | (test_dat.label==2) 
+# test_dat.loc[slt2,"label"] -= 1 #merge e-gamma and shift muon label
+test_dat.loc[slt,"label"] -= 2 #3 #1 #shift  pi+ labels
 test_dat.reset_index(drop=True,inplace=True)
 
-x_test, pid_test, en_test = [],[], []
-for i in range(len(test_dat)):
-#     print('Event number {}'.format(i))
-    x_test.append(test_dat.loc[i].feature)
-    pid_test.append(test_dat.loc[i].label)
-    en_test.append(test_dat.loc[i].gen_energy)
+x_test, pid_test, en_test = [],[],[]
+
+x_test.append(test_dat.feature)
 x_test = np.array(x_test)
+x_test = x_test[0]
+
+pid_test.append(test_dat.label)
 pid_test = np.array(pid_test)
-en_test = np.array(en_test)
+pid_test = pid_test[0]
 pid_test = keras.utils.to_categorical(pid_test, num_classes=classes, dtype='float32')
-print('Test data loaded!')
+
+en_test.append(test_dat.gen_energy)
+en_test = np.array(en_test)
+en_test = en_test[0]
+
+# for i in range(len(test_dat)):
+#     x_test.append(test_dat.loc[i].feature)
+#     pid_test.append(test_dat.loc[i].label)
+#     en_test.append(test_dat.loc[i].gen_energy)
+# x_test = np.array(x_test)
+# pid_test = np.array(pid_test)
+# en_test = np.array(en_test)
+# pid_test = keras.utils.to_categorical(pid_test, num_classes=classes, dtype='float32')
+# print('Test data loaded!')
+
+print(x_test.shape)
+print(pid_test.shape)
+print(en_test.shape)
 
 #class_names = np.array(['gamma', 'electron', 'muon', 'tau', 'pion_n', 'pion_c'])
-class_names = np.array(['gamma', 'electron', 'muon', 'pion_n', 'pion_c'])
+# class_names = np.array(['gamma', 'electron', 'muon', 'pion_n', 'pion_c'])
+# class_names = np.array(['electromagnetic', 'muon', 'pion_n', 'pion_c'])
+class_names = np.array(['gamma', 'electron', 'muon', 'pion_c'])
 
 # load model
 model = load_model(save_dir + model_name + '.h5')
@@ -53,6 +88,7 @@ model = load_model(save_dir + model_name + '.h5')
 # score trained model
 scores = model.evaluate(x_test, pid_test, verbose=1)
 y_pred = model.predict(x_test)
+print('y_pred shape: ', y_pred.shape)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
 
@@ -60,12 +96,14 @@ true = np.argmax(pid_test, axis=1)
 pred = np.argmax(y_pred, axis=1)
 print('True labels: ', true)
 print('Predicted labels: ', pred)
+print(np.unique(true))
+print(np.unique(pred))
 
 def plot_confusion_matrix(cm, classes, normalize=True, title='Confusion matrix', cmap=plt.cm.Blues):
     
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
+        print("Confusion matrix")
     else:
         print('Confusion matrix, without normalization')
 
@@ -93,7 +131,7 @@ np.set_printoptions(precision=2)
 
 # Plot normalized confusion matrix
 plt.figure()
-plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True, title='Normalized confusion matrix')
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True, title='Confusion matrix')
 
 plt.savefig(plotdir + 'confusion_matrix.png')
 plt.show()
--- train.py
+++ train.py
@@ -11,7 +11,6 @@ from keras.models import Model
 from keras.callbacks import EarlyStopping
 from keras.layers import Input, Conv2D, MaxPooling2D, Concatenate, BatchNormalization, Flatten, Dense, Dropout
 from keras import optimizers
-
 import os
 import numpy as np
 import pandas as pd
@@ -23,12 +22,14 @@ batch_size = 256
 channels = 3
 img_height = 30
 img_width = 50
-classes = 5 #6
-epochs = 15
+classes = 4 #5 #6
+epochs = 100 #15
 dataset_dir = 'data/pkl/'
 save_dir = 'saved_models/'
-model_name = 'cnn_v1'
-history_name = 'cnn_v1_history'
+model_name = 'full_model'
+history_name = 'full_model_history'
+# model_name = 'pid'
+# history_name = 'pid_history'
 
 # Load the data
 train_dat = pd.read_pickle(dataset_dir + "dataset_train.pkl")
@@ -36,42 +37,80 @@ test_dat = pd.read_pickle(dataset_dir + "dataset_test.pkl")
 
 print('Loading training data...')
 train_dat = train_dat.query("label!=3")
-slt = train_dat.label>3
-train_dat.loc[slt,"label"] -= 1
+train_dat = train_dat.query("label!=4")
+# slt = train_dat.label>3
+slt = train_dat.label==5
+# slt2 = (train_dat.label==1) | (train_dat.label==2) 
+# train_dat.loc[slt2,"label"] -= 1
+train_dat.loc[slt,"label"] -= 2 #3 #1
 train_dat.reset_index(drop=True,inplace=True)
 
-x_train, pid_train, en_train = [],[], []
-for i in range(len(train_dat)):
-#     print('Event number {}'.format(i))
-    x_train.append(train_dat.loc[i].feature)
-    pid_train.append(train_dat.loc[i].label)
-    en_train.append(train_dat.loc[i].gen_energy)
+x_train, pid_train, en_train = [],[],[]
+
+x_train.append(train_dat.feature)
 x_train = np.array(x_train)
+x_train = x_train[0]
+
+pid_train.append(train_dat.label)
 pid_train = np.array(pid_train)
-en_train = np.array(en_train)
+pid_train = pid_train[0]
 pid_train = keras.utils.to_categorical(pid_train, num_classes=classes, dtype='float32')
-print('Training data loaded!')
+
+en_train.append(train_dat.gen_energy)
+en_train = np.array(en_train)
+en_train = en_train[0]
+
+# for i in range(len(train_dat)):
+# #     print('Event number {}'.format(i))
+#     x_train.append(train_dat.loc[i].feature)
+#     pid_train.append(train_dat.loc[i].label)
+#     en_train.append(train_dat.loc[i].gen_energy)
+# x_train = np.array(x_train)
+# pid_train = np.array(pid_train)
+# en_train = np.array(en_train)
+# print("Values of pid: ", np.unique(pid_train))
+# pid_train = keras.utils.to_categorical(pid_train, num_classes=classes, dtype='float32')
+# print('Training data loaded!')
 
 print(x_train.shape)
 print(pid_train.shape)
 print(en_train.shape)
 
 print('Loading test data...')
-test_dat = test_dat.query("label!=3")
-slt = test_dat.label>3
-test_dat.loc[slt,"label"] -= 1
+test_dat = test_dat.query("label!=3") #drop tau
+test_dat = test_dat.query("label!=4") #drop pi0
+# slt = test_dat.label>3
+slt = test_dat.label==5
+# slt2 = (test_dat.label==1) | (test_dat.label==2) 
+# test_dat.loc[slt2,"label"] -= 1 #merge e-gamma and shift muon label
+test_dat.loc[slt,"label"] -= 2 #3 #1 #shift  pi+ labels
 test_dat.reset_index(drop=True,inplace=True)
 
-x_test, pid_test, en_test = [],[], []
-for i in range(len(test_dat)):
-    x_test.append(test_dat.loc[i].feature)
-    pid_test.append(test_dat.loc[i].label)
-    en_test.append(test_dat.loc[i].gen_energy)
+x_test, pid_test, en_test = [],[],[]
+
+x_test.append(test_dat.feature)
 x_test = np.array(x_test)
+x_test = x_test[0]
+
+pid_test.append(test_dat.label)
 pid_test = np.array(pid_test)
-en_test = np.array(en_test)
+pid_test = pid_test[0]
 pid_test = keras.utils.to_categorical(pid_test, num_classes=classes, dtype='float32')
-print('Test data loaded!')
+
+en_test.append(test_dat.gen_energy)
+en_test = np.array(en_test)
+en_test = en_test[0]
+
+# for i in range(len(test_dat)):
+#     x_test.append(test_dat.loc[i].feature)
+#     pid_test.append(test_dat.loc[i].label)
+#     en_test.append(test_dat.loc[i].gen_energy)
+# x_test = np.array(x_test)
+# pid_test = np.array(pid_test)
+# en_test = np.array(en_test)
+# print("Values of pid: ", np.unique(pid_test))
+# pid_test = keras.utils.to_categorical(pid_test, num_classes=classes, dtype='float32')
+# print('Test data loaded!')
 
 print(x_test.shape)
 print(pid_test.shape)
@@ -81,24 +120,37 @@ print(en_test.shape)
 def shower_classification_model():
         input_img = Input(shape=(img_width, img_height, channels), name='input')
 
-        conv = Conv2D(3, (1,1), padding='same', activation='relu', data_format='channels_last', name='conv1')(input_img)
-        conv = Conv2D(8, (5,1), activation='relu', padding='same', data_format='channels_last', name='conv2')(conv)
-        conv = Conv2D(8, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv3')(conv)
-        conv = Conv2D(16, (4,4), activation='relu', padding='same', data_format='channels_last', name='conv4')(conv)
+        flat = Flatten()(input_img)
+        dense = Dense(1024, activation='relu', name='dense1')(flat)
+        dense = Dense(128, activation='relu', name='dense2')(dense)
+        dense = Dense(32, activation='relu', name='dense3')(dense)
+        
+        # conv = Conv2D(3, (1,1), padding='same', activation='relu', data_format='channels_last', name='conv1')(input_img)
+        # conv = Conv2D(5, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv1')(input_img)
+        # pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool1')(conv)
 
-        flat = Flatten()(conv)
-        bnorm = BatchNormalization()(flat)
+        # conv = Conv2D(5, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv2')(pool)
+        # pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool2')(conv)
 
-        dense = Dense(1024, activation='relu', name='dense1')(bnorm) #(flat)
-        drop = Dropout(0.5)(dense)
-        dense = Dense(128, activation='relu', name='dense2')(drop)
-        drop = Dropout(0.5)(dense)
+        # conv = Conv2D(5, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv3')(pool)
+        # pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool3')(conv)
 
-        pred = Dense(classes, activation='softmax', name='output')(drop)
+        # conc = Concatenate(axis=3)([conv,conv2])
+        # conv = Conv2D(3, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv4')(conv)
+
+        # flat = Flatten()(pool)
+        # bnorm = BatchNormalization()(flat)
+
+        # dense = Dense(32, activation='relu', name='dense1')(flat)
+        # drop = Dropout(0.5)(dense)
+        # dense = Dense(32, activation='relu', name='dense2')(drop)
+        # drop = Dropout(0.5)(dense)
+
+        pred = Dense(classes, activation='softmax', name='output')(dense)
 
         model = Model(inputs=input_img, outputs=pred)
 
-        opt = optimizers.Adam(lr=0.01)
+        # opt = optimizers.Adam(lr=0.01)
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
 
@@ -122,23 +174,92 @@ def shower_classification_model_V2():
         # bnorm = BatchNormalization()(flat)
 
         dense = Dense(1024, activation='relu', name='dense1')(flat)
-        # drop = Dropout(0.5)(dense)
+        drop = Dropout(0.5)(dense)
         dense = Dense(128, activation='relu', name='dense2')(drop)
-        # drop = Dropout(0.2)(dense)
+        drop = Dropout(0.2)(dense)
 
         pred = Dense(classes, activation='softmax', name='output')(drop)
 
         model = Model(inputs=input_img, outputs=pred)
 
-        opt = optimizers.Adam(lr=0.01)
-        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+        # opt = optimizers.Adam(lr=0.01)
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
+
+def pid_and_er_model():
+        input_img = Input(shape=(img_width, img_height, channels), name='input')
+
+        conv = Conv2D(3, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv1')(input_img)
+        # pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool1')(conv)
+
+        conv = Conv2D(3, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv2')(conv) #(pool)
+        # pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool2')(conv)
+
+        conv = Conv2D(3, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv3')(conv) #(pool)
+        # pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool3')(conv)
+        # conc = Concatenate(axis=3)([conv,conv2])
+        # conv = Conv2D(3, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv4')(conv)
+
+        flat = Flatten()(conv)
+        # bnorm = BatchNormalization()(flat)
+
+        dense = Dense(1024, activation='relu', name='dense1')(flat)
+        # drop = Dropout(0.5)(dense)
+        # dense = Dense(32, activation='sigmoid', name='dense2')(drop)
+        # drop = Dropout(0.5)(dense)
+        pid = Dense(classes, activation='softmax', name='pid_output')(dense)
+
+        conc = Concatenate()([flat,pid])
+        bnorm = BatchNormalization()(conc)
+
+        dense = Dense(1024, activation='relu', name='dense2')(bnorm)
+        # dense = Dense(128, activation='relu', name='dense3')(dense)        
+        # dense = Dense(16, activation='relu', name='dense4')(dense)        
+        enreg = Dense(1, name='enreg_output')(dense)
+
+        model = Model(inputs=input_img, outputs=[pid, enreg])
+
+        # opt = optimizers.Adam(lr=0.01)
+        model.compile(loss={'pid_output': 'categorical_crossentropy', 'enreg_output': 'mse'}, loss_weights={'pid_output': 1., 'enreg_output': 0.000001}, optimizer='adam', metrics={'pid_output': 'accuracy', 'enreg_output': 'mse'})
+        return model
+
+def energy_regression_model():
+        input_img = Input(shape=(img_width, img_height, channels), name='input')
+
+        # conv = Conv2D(3, (1,1), padding='same', activation='relu', data_format='channels_last', name='conv1')(input_img)
+        conv = Conv2D(5, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv1')(input_img)
+        pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool1')(conv)
+
+        conv = Conv2D(5, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv2')(pool)
+        pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool2')(conv)
+
+        conv = Conv2D(5, (3,3), activation='relu', padding='same', data_format='channels_last', name='conv3')(pool)
+        pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_last', name='pool3')(conv)
+ 
+        flat = Flatten()(pool)
+        # bnorm = BatchNormalization()(flat)
+
+        dense = Dense(256, activation='relu', name='dense1')(flat)
+        drop = Dropout(0.5)(dense)
+        dense = Dense(64, activation='relu', name='dense2')(drop)
+        drop = Dropout(0.5)(dense)
+
+        enreg = Dense(1, name='enreg_output')(drop)
+
+        model = Model(inputs=input_img, outputs=enreg)
+
+        opt = optimizers.Adam(lr=0.001, decay=1e-3/200)
+        model.compile(loss='mean_absolute_percentage_error', optimizer=opt) #,metrics=['mse'])
         return model
 
 print('Creating model...')
 model = shower_classification_model()
+# model = pid_and_er_model()
 model.summary()
 
 history = model.fit(x_train, pid_train, batch_size=batch_size, epochs=epochs, validation_split=0.1, callbacks=[EarlyStopping(monitor='val_loss', patience=10, verbose=1, restore_best_weights=True)], shuffle=True, verbose=1)
+
+# history = model.fit(x_train, {'pid_output': pid_train, 'enreg_output': en_train}, batch_size=batch_size, epochs=epochs, validation_split=0.1, callbacks=[EarlyStopping(monitor='val_loss', patience=10, verbose=1, restore_best_weights=True)], shuffle=True, verbose=1)
 history_save = pd.DataFrame(history.history).to_hdf(save_dir + history_name + ".h5", "history", append=False)
 
 
@@ -172,7 +293,28 @@ print('Model saved')
 # Score trained model
 
 scores = model.evaluate(x_test, pid_test, verbose=1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
+# scores = model.evaluate(x_test, {'pid_output': pid_test, 'enreg_output': en_test}, verbose=1)
+
+print('Creating second model...')
+model_2 = energy_regression_model()
+model_2.summary()
+
+history = model_2.fit(x_train, en_train, batch_size=batch_size, epochs=epochs, validation_split=0.1, shuffle=True, verbose=1)
+
+results = model_2.predict(x_test)
+print('*****************')
+print('True Particle Energies= {} '.format(en_test))
+print('Predicted Particle Energies= {}'.format(results))
+
+# print('True Particle 0 PID= {}'.format(np.argmax(pid_test, axis=1)))
+# print('Predicted Particle 0 PID= {}'.format(np.argmax(results[0], axis=1)))
+# print('Predicted Particle 0 Energy= {}'.format(results[1]))
+# # print('Test loss:', scores[0])
+# # print('Test accuracy:', scores[1])
+# print('*************')
+# print('Scores:', scores)
+# mse = (np.square(en_test - results[1])).mean(axis=None)
+# print('MSE: ', mse)
+
 
 print('Done!')
